64a7bfa3d7b1c04820a5a2548de39630e182316f
Alejandro Valenzuela <tzalejo@gmail.com>
2024-02-24 12:09:02 -0300

    hicimos suplantacion de ciertas dependencia

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,9 +1,6 @@
 const BooksService = require('./books.service');
 
-
-// jest fn es quien nos deja hace mock, test d comportamiento, simular y espiar..
-const mockGetAll = jest.fn();
-
+// para simular datos que devolveria una base de datos..
 const fakeBooks = [
   {
     _id: 1,
@@ -11,61 +8,51 @@ const fakeBooks = [
   },
 ];
 
+// esto estaria suplantando a nuestra clase MongoLib
+// para ello hay q suplantar todo los comportamiento,
+// es decir, todo los que BooksService este utilizando
+// en este casoi .getAll y .create
 const MongoLibStub = {
-  getAll: mockGetAll, //() => [...fakeBooks],
+  getAll: () => [...fakeBooks],
   create: () => { },
 };
 
-jest.mock('./../lib/mongo.lib', () => jest.fn().mockImplementation(() => {
-  return {
-    getAll: mockGetAll, //() => [...fakeBooks],
-    create: () => { },
-
-  };
-}));
+// Se utilizo el método jest.mock, al cual se le indica la clase que va
+// suplantar (lib/mongo.lib) por  MongoLibSub
+// el cual MongoLibSub ya tiene los metodos con respuestas ya preparada
+jest.mock('./../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
 
 describe('Test for BooksService', () => {
   let service;
 
   beforeEach(() => {
+    // para cada prueba , va craer una instancia nueva
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
-  describe('Test for Books Service', () => {
+  describe('Test for getBooks', () => {
     test('should return a list books', async () => {
       // Arrange: aca tengo mokear todo las funcionalidades que implica ir a la bd
-      mockGetAll.mockResolvedValue(fakeBooks);
-
-      // y traer informacion, osea los servicios externos.
-
 
       // act
       const books = await service.getBooks({});
 
       // assert
       expect(books.length).toEqual(1);
-      expect(mockGetAll).toHaveBeenCalled();
-      expect(mockGetAll).toHaveBeenCalledWith('books', {});
-
     });
-
 
     test('should return a list books 2', async () => {
       // Arrange: aca tengo mokear todo las funcionalidades que implica ir a la bd
-      mockGetAll.mockResolvedValue([{
-        _id: 1,
-        name: 'Harry Potter II',
-      },
-      ]);
 
       // y traer informacion, osea los servicios externos.
-
 
       // act
       const books = await service.getBooks({});
 
       // assert
-      expect(books[0].name).toEqual('Harry Potter II');
+      expect(books[0].name).toEqual('Harry Potter');
     });
-  })
+
+  });
 });
